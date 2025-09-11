6ce010f93b83f8998c47d5393d9d0006622435d8
mohamedSayed.tourabi <mohamedsayed.tourabi@maibornwolff.de>
2025-09-11 17:36:59 +0100

    fix create-infrastructure.yml

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -18,27 +18,6 @@ provider "azurerm" {
   # ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID, ARM_SUBSCRIPTION_ID
 }
 
-# Register required Azure Resource Providers
-resource "azurerm_resource_provider_registration" "container_registry" {
-  name = "Microsoft.ContainerRegistry"
-}
-
-resource "azurerm_resource_provider_registration" "container_service" {
-  name = "Microsoft.ContainerService"
-}
-
-resource "azurerm_resource_provider_registration" "compute" {
-  name = "Microsoft.Compute"
-}
-
-resource "azurerm_resource_provider_registration" "network" {
-  name = "Microsoft.Network"
-}
-
-resource "azurerm_resource_provider_registration" "storage" {
-  name = "Microsoft.Storage"
-}
-
 # Local values for common tags
 locals {
   common_tags = {
