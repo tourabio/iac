049fb97a6c8c12c4aca27ec44928d44cc3176644
mohamedSayed.tourabi <mohamedsayed.tourabi@maibornwolff.de>
2025-09-11 15:58:47 +0100

    fix create-infrastructure.yml

--- infrastructure/main.tf
+++ infrastructure/main.tf
@@ -13,6 +13,9 @@ provider "azurerm" {
   features {}
   subscription_id            = var.subscription_id
   skip_provider_registration = true
+  
+  # Use environment variables for authentication in GitHub Actions
+  # ARM_CLIENT_ID, ARM_CLIENT_SECRET, ARM_TENANT_ID, ARM_SUBSCRIPTION_ID
 }
 
 # Register required Azure Resource Providers
